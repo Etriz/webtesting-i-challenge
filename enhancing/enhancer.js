f0455c56b772d3730e557ad412bbe0622fb77196
ryanp <ryanpanimation@gmail.com>
2020-08-11 00:27:38 -0500

    working mvp tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,16 +5,29 @@ module.exports = {
   get,
 };
 
+function repair(item) {
+  let newItem = { ...item };
+  newItem.durability = 100;
+  return newItem;
+}
+
 function success(item) {
-  return { ...item };
+  let newItem = { ...item };
+  newItem.enhancement !== 20 ? (newItem.enhancement += 1) : null;
+  return newItem;
 }
 
 function fail(item) {
-  return { ...item };
-}
-
-function repair(item) {
-  return { ...item };
+  let newItem = { ...item };
+  if (newItem.enhancement < 15) {
+    newItem.durability -= 5;
+  } else if (newItem.enhancement > 16) {
+    newItem.durability -= 10;
+    newItem.enhancement -= 1;
+  } else if (newItem.enhancement >= 15) {
+    newItem.durability -= 10;
+  }
+  return newItem;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,30 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+test("initial test", () => {
+  expect(true).toBe(true);
+});
+
+describe("enhancement fns", () => {
+  let item = {};
+  beforeEach(() => {
+    item.name = "Great Sword";
+    item.durability = 20;
+    item.enhancement = 10;
+    return item;
+  });
+  test("repair", () => {
+    expect(enhancer.repair(item)).toStrictEqual({ ...item, durability: 100 });
+  });
+  test("success", () => {
+    expect(enhancer.success(item)).toStrictEqual({ ...item, enhancement: 11 });
+  });
+  test("fail", () => {
+    expect(enhancer.fail(item)).toStrictEqual({ ...item, durability: 15 });
+    item.durability = 20;
+    item.enhancement = 17;
+    expect(enhancer.fail(item)).toStrictEqual({ ...item, durability: 10, enhancement: 16 });
+    item.durability = 20;
+    item.enhancement = 15;
+    expect(enhancer.fail(item)).toStrictEqual({ ...item, durability: 10 });
+  });
+});
